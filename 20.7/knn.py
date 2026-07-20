7ba003b6ee501abdcb2b08a906d4f6121dcb383a
Bashitha267 <nimeshspc2k17@gmail.com>
2026-07-20 23:11:07 +0530

    added July 20 practical

---
+++ 20.7/knn.py
@@ -0,0 +1,9 @@
+import math
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.matrics import accuracy_score,confusion_matrix,ConfusionMatrixDisplay
+
+git config --global user.email "nimeshspc2k17@gmail.com"
+git config --global user.name "Bashitha267"
